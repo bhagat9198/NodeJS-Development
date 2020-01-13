b8b36dcd4664f9ac6390f20ed27c7208c9d51603
Bhagat Singh <bhagatsingh.17is@saividya.ac.in>
2020-01-13 23:54:19 +0530

    complete ejs engine

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ const path =  require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-// handlesbars are not like 'pug', so we have to explicity tell express about "handlebars"
-// to check if your teamplating engin is auto supported by express, just check the docs regarding that
-const expressHbs = require('express-handlebars');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -12,19 +9,9 @@ const rootDir = require('./util/path');
 
 const app = express();
 
-// after registering, we have to tell, express "handlebars" is templet engin and it exits, we do
-// "engin()" is method which helps in registering templating engin which is not buit--in like pug
-// "handlebars" is the engine name, which we wish to call(we can give any name)
-// "expressHbs" is templating engine, which we have registered above, and we have to call it
-// app.engine('handlebars', expressHbs());
-
-// "extname: 'hbs'" : to tell main layout file has .hbs extenstion, this argument only to specify about layout file.active
-//  "layoutsDir: 'views/layouts/'" : to tell the path of layout folder, by default it uses "views/layouts/" path
-// "defaultLayout: 'mainContent'" : to tell the name of layiut file, bydefault it will use "main.handlebars" file name.
-app.engine('hbs', expressHbs({extname: 'hbs', layoutsDir: 'views/layouts/', defaultLayout: 'mainContent'}));
-app.set('view engine', 'hbs');
+// no need to register the engin, as exoress auto register it
+app.set('view engine', 'ejs');
 app.set('views', 'views');
-
 
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.static(path.join(rootDir, 'public')));
@@ -36,7 +23,8 @@ app.use((req, res, next) => {
   res.status(404);
   res.setHeader('Content-type','text/html');
   // passing the argument to templating engine doesnt change, it remains the same
-  res.render('404', {pageTitle: "404"});
+  // "path: 'Error'" we passed becasue ejs engin will check "path" argument in naviagation bar logic of template. if we will not provide, we will get an error
+  res.render('404', {pageTitle: "404", path: 'Error'});
 });
 
 app.listen(3000);
