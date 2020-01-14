3b3ca6333a31aef6472fffb0b5a07d9698c61704
Bhagat Singh <bhagatsingh.17is@saividya.ac.in>
2020-01-14 19:18:42 +0530

    added error controller

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const rootDir = require('./util/path');
+const errorController = require('./controller/error');
 
 const app = express();
 
@@ -18,10 +19,6 @@ app.use(express.static(path.join(rootDir, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404);
-  res.setHeader('Content-type','text/html');
-  res.render('404', {pageTitle: "404", path: 'Error'});
-});
+app.use(errorController.get404);
 
 app.listen(3000);
