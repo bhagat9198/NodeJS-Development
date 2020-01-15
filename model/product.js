865cba4cb7aed8f7453453ced4bb4c72f4cdb7b8
Bhagat Singh <bhagatsingh.17is@saividya.ac.in>
2020-01-15 11:25:56 +0530

    storing data in a file via model

--- model/product.js
+++ model/product.js
@@ -1,39 +1,89 @@
-// ES5 way
-// module.exports = function Product() {}
+// no more array, as we will store all our products in a file
+// const products = [];
 
+const fs = require('fs');
+const path = require('path');
 
-const products = [];
+const rootDir = require('../util/path');
 
-// ES6 : using concept of classes
 module.exports = class Product {
 
-  // constructor is used to give shape to the product
   constructor(t) {
-    // creating a property within a class "title"
-    // this allows us to create an object based in this class where we can pass the ttitle to the constructor, which we call with new and then this will get stored in created object
-    // but we dont only create the object 
     this.title = t;
   }
-
-  // but we want to store the product to our array product and fetch it when nessary
-
-  // "save" is special function without a 'function' keyword
-  // save method is avaliable in the class where we will save the products
-
-  // "save()" should be called on instatited object pased on object
   save() {
-    // "this" will reffer to the object created within a class, and that object we want to push to the product array
-    products.push(this);
+    // no more pushing elements to array
+    // products.push(this);
+
+    // creating a file
+    // if u dont want to use, "path" file which is in util folder, directly use the command, which we have provided in "path" file of util folder
+    // const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
+
+    // we will store file in JSON format
+    const p = path.join(rootDir, 'data', 'products.json')
+
+    // to store the new product, first we need to get existing products and then store new product at the end
+    // so, first we read the file
+    // for large file, we will not read file.ie, not putting the file in memory. we will be reading as a stream
+    // fs.createReadStream()
+
+    // but our file will be shorter, we will read the file
+    // function in second argument will be executed when reading of file is done 
+    fs.readFile(p, (err, fileContent) => {
+      // console.log(fileContent);
+
+      //if we run the server, we will get 
+      // ReferenceError: products is not defined  at "shop.js"
+      // we got an error because, we dont have any file... so no content
+
+      // now, if we see error 
+      // console.log(err);
+      // in console, we can see
+      // "undefined"
+      // [Error: ENOENT: no such file or directory, open 'D:\My\vscode\node\5.express\data\products.json'] {
+      //   errno: -4058,
+      //   code: 'ENOENT',
+      //   syscall: 'open',
+      //   path: 'D:\\My\\vscode\\node\\5.express\\data\\products.json'
+      // }
+
+
+      let products = [];
+      if (!err) {
+        // parse method will takes incoming JSON data and gives back js array or object or whatever is there in a file
+        products = JSON.parse(fileContent);
+      }
+      // as we are using arrow function, it will reffer to the class. if we will use "function"(es5), it will losse its context and will not reffer to the class
+      // "this" refferes to the current object of the class 
+      products.push(this);
+
+      // after pushing to array, now we should save it to the file
+      // "stringify" method will take js object/arary and convert it into JSON format
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+        // we will still get an error, as we are not outputting anything. but we get 
+        // "null"
+        // but in data folder, "products.json" is craeted where we can see the one object created in form of JSON
+
+      });
+    })
   }
 
-  // after saving, we should able to retrive the products from the array product
-  // "fetchAll" method is like utility function. this is not called on single instance of the product because because it should fetch all products and we dont want to create new object with new keyword with some dummy title, just to fech all existence product.
-  // thus we will add "static " keyword, which make sure that we call this method directly on the class itself and no on any instanttiated object
   static fetchAll() {
-    // "this.product" will return the current product and not the full array of products
-    // return this.product;
+    const p = path.join(rootDir, 'data', 'products.json')
 
-    // we should return whole product array
-    return products;
+    // after stroing it in file, we should reterive the data
+    fs.readFile(p, (err, fileContent) => {
+      if(err) {
+        // if error is there, it will simply gives empty array and comes out of function
+        return [];
+      }
+      // convert json to JS form and pront it out
+      return JSON.parse(fileContent);
+    });
+
+    // running the server, we get 
+    // Cannot read property 'length' of undefined
+    // this error can be hard to debug
   }
 }
