603f66e8bd9d6f8a5018abd743620d4b686b023e
Bhagat Singh <bhagatsingh.17is@saividya.ac.in>
2020-01-15 14:06:39 +0530

    099 Refactoring the File Storage Code

--- model/product.js
+++ model/product.js
@@ -3,63 +3,45 @@ const path = require('path');
 
 const rootDir = require('../util/path');
 
-module.exports = class Product {
+// now we will refactore the code, ie reuse the code(or improve the exisiting code)
+const p = path.join(rootDir, 'data', 'products.json');
 
+// craerting common function to extrat data from file
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    // as we want only one command to exetue based on condition, not both
+    // if(err) {
+    //   cb([]);  
+    // } else {
+    //   cb(JSON.parse(fileContent));
+    // }
+    // OR
+    if(err) {
+      return cb([]);  
+    } 
+      return cb(JSON.parse(fileContent));
+
+  });
+}
+
+
+module.exports = class Product {
+  
   constructor(t) {
     this.title = t;
   }
+
   save() {
-    const p = path.join(rootDir, 'data', 'products.json')
-    let products = [];
-    fs.readFile(p, (err, fileContent) => {
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
-        products.push(this);
+    getProductsFromFile((products) => {
+      products.push(this);
   
       fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err);
-  
       });
     })
-    
   }
 
-  // this function is running asyn code
-  // it will store both the function but will not execute 1.const p = path.join 2.fs.readFile(p, (err, fileContent), thus function does not return anything
-  // hence, undefined will be passed from fetchAll(). thus in "shop.ejs", after passing value to shop.ejs. our condition becaome (undefined.length > 0)
-
-  // static fetchAll() {
-  //   const p = path.join(rootDir, 'data', 'products.json')
-
-  //   fs.readFile(p, (err, fileContent) => {
-  //     if(err) {
-  //       return [];
-  //     }
-  //     return JSON.parse(fileContent);
-  //   });
-
-  // FIX
-  // more info on callback handling: https://www.youtube.com/watch?v=jgWnccjXR4I
-  // we are passing callback function, that actually allows me to pass a function into fetchAll() which fechAll will execute once it is done, so that thing calling fetchAll() can pass a function. it is then aware of being called which holds the data, we want to return
-
-  //1. so we are reciving a function(cb), this argument will hold a function 
   static fetchAll(cb) {
-    const p = path.join(rootDir, 'data', 'products.json')
-
-    // expalation: readfile also takes a callback,we pass the fuction, which should be executed once it is done
-    fs.readFile(p, (err, fileContent) => {
-    if(err) {
-      // 2.and therefoe instaed of returning an array, here we will execute callback function, which will pass empty array
-      cb([]);  
-    }
-    // 3. same as step 2
-    cb(JSON.parse(fileContent));
-  });
-    // running the server, we get 
-    // Cannot read property 'length' of undefined
-    // this error can be hard to debug
-
-
+    getProductsFromFile(cb);
   }
 }
