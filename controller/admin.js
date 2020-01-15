0da04e751ec6b406aa40b35459d88bba1551fba3
Bhagat Singh <bhagatsingh.17is@saividya.ac.in>
2020-01-15 17:52:22 +0530

    106 Storing Product Data

--- controller/admin.js
+++ controller/admin.js
@@ -8,7 +8,12 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  const product = new Product(req.body.title);
+  // const product = new Product(req.body.title);
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const description = req.body.description;
+  const price = req.body.price;
+  const product = new Product(title, imageUrl, description, price);
   product.save();
 
   console.log("admin.js ", product);
--- model/product.js
+++ model/product.js
@@ -27,10 +27,13 @@ const getProductsFromFile = (cb) => {
 
 module.exports = class Product {
   
-  constructor(t) {
-    this.title = t;
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
-
+  
   save() {
     getProductsFromFile((products) => {
       products.push(this);
