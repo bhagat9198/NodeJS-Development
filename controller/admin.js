7efdd0fcaf24cf948ef9862cee76beb81ec5aebb
Bhagat Singh <bhagatsingh.17is@saividya.ac.in>
2020-01-16 22:46:57 +0530

    121 Pre-Populating the Edit Product Page with Data

--- controller/admin.js
+++ controller/admin.js
@@ -4,6 +4,7 @@ exports.getAddProduct = (req, res, next) => {
   res.render("admin/edit-product", {
     path: "/admin/add-product",
     pageTitle: "Add Products",
+    editing: false
   });
 };
 
@@ -20,34 +21,24 @@ exports.postAddProduct = (req, res, next) => {
 };
 
 exports.getEditProduct = (req, res, next) => {
-  // now lets say, we need to get additional confirmation by ensuring that people have to pass us so called "query parameters"in a url.
-  // a query parameter can be added to any url by adding a question mark and then a key,value pair seprated by equal to sign(keyValue = valueValue)
-  // in our case, Eg: http://localhost:3000/admin/edit-product/12?edit=true
-  // and we can have multiple query parameters by seprating them with ampersand sign(&)
-  // Eg: http://localhost:3000/admin/edit-product/12?edit=true&title=new
-  // this "edit=true&title=new" is called as optinal data. this data is not to be in routes file, where we determine route for different files
-  // but, we can check for query paramerters in our controllers
-  // this is done by:-
-
-  // "query" is inbuild propertly given by express
-  // here we are checking key "edit" if it is passed in optianl data
-  // so if "edit" value is set somewhere in query parameters, we will get response as "true"
-  // the extrated value (ie, response) is always a string. so it will be "true" not true(bool value)
-  // if it didnt get the key "edit", it will set to undefined that is false
   const editMode = req.query.edit
-  // thus, now we will enter edit mode, only if it editMode is set, which we have done
-  
   if(!editMode) {
-    // well, it is redundant thing here. as we already know we are editing new product here
     res.redirect('/');
   }
 
-  // so query parameters are used to tracking down users or some filters set on the page
-  res.render("admin/edit-product", {
-    path: "/admin/edit-product",
-    pageTitle: "Edit Products",
-    editing: editMode
+  // taking out productID from the dynamic parameters which we are passing in url
+  const productID = req.params.productID;
+  // console.log(productID);
+
+  Product.findById(productID, product => {
+    res.render("admin/edit-product", {
+      path: "/admin/edit-product",
+      pageTitle: "Edit Products",
+      editing: editMode,
+      product: product
+    });
   });
+  
 };
 
 exports.getProducts = (req, res, next) => {
