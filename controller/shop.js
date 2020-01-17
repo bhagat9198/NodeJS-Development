19f1f18417a6057101a8886f3531e3d217ced614
Bhagat Singh <bhagatsingh.17is@saividya.ac.in>
2020-01-17 22:32:07 +0530

    127 Deleting Cart Items

--- controller/shop.js
+++ controller/shop.js
@@ -13,7 +13,6 @@ exports.getProduct = (req, res, next) => {
 
 exports.getDetails = (req, res, next) => {
   const productID = req.params.productId;
-  // console.log(productID);
   Product.findById(productID, product => {
     res.render('shop/product-detail', { pageTitle: product.title, product: product, path: '/products' });
   });
@@ -21,30 +20,18 @@ exports.getDetails = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  // passing a callback function, once successful, it will get an cart items
   Cart.getCart(cart => {
-    // along with the cart, we need product details like title. as cart is only having an product id and quantity.
-    // so, to get thr product details
     Product.fetchAll(products => {
-      // now from fetchAll method, we will get all the products. if have to find which products are in cart. thus going through all the products
       const cartProducts = [];
       for(product of products) {
-        // thus checking if present product is in cart
-        // if(cart.products.find(prod => prod.id === product.id)) {
-        //   // if the product is in cart, then add it to "cartProduts" array
-        //   cartProducts.push(product);
-        //   // but along with product details, we need quantity of product too. quantity is strord in cart, hence we need to extract that too.
-        // }
         const CartproductData = cart.products.find(prod => prod.id === product.id)
         if(CartproductData) {
           cartProducts.push({productData: product, qty: CartproductData.qty});
-          // thus, we have product details and quantity of a product
         }
       }
       res.render('shop/cart', {
         pageTitle: 'Cart',
         path: '/cart',
-        // cartProducts is an array which is having both product details and porduct quantity
         products: cartProducts
       });
     }); 
@@ -54,8 +41,6 @@ exports.getCart = (req, res, next) => {
 };
 
 exports.postCart = (req, res, next) => {
-  // as request was "POST", so it contains the productID as agrument within the url of "form" 
-  // "productID" is the name given to the value ID, in the form field.
   const productID = req.body.productID
   console.log(productID);
   Product.findById(productID, product => {
@@ -63,6 +48,19 @@ exports.postCart = (req, res, next) => {
   })
   res.redirect('/');
 }
+
+exports.postCartDeleteProduct = (req, res, next) => {
+  // while deleting, we have delete product only from the cart and not product as a whole from everywhere
+  // getting the productID which we get it from post request while deleting the item
+  const productID = req.body.productID;
+  // finding the product
+  Product.findById(productID, product => {
+    // after getting the product, we will get to know the product price, which can be sent to cart to delete an item.
+    // we can even directly send product price along with productID from the form. but its a good practice.thus using " Product.findById" method to extract product details
+    Cart.deleteProduct(productID, product.price);
+    res.redirect('/cart');
+  });
+};
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
--- routes/shop.js
+++ routes/shop.js
@@ -16,6 +16,8 @@ routes.get('/cart', shopController.getCart);
 
 routes.post('/cart', shopController.postCart);
 
+routes.post('/cart-delete-item', shopController.postCartDeleteProduct);
+
 routes.get('/orders', shopController.getOrders);
 
 module.exports = routes;
