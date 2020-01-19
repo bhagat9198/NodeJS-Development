6fa0064164cadbfd370d14ec8e878d4a68a297d7
Bhagat Singh <bhagatsingh.17is@saividya.ac.in>
2020-01-19 21:15:35 +0530

    149 Syncing JS Definitions to the Database

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const rootDir = require('./util/path');
 const errorController = require('./controller/error');
-const db = require('./util/database');
+// const db = require('./util/database');
+const sequelize = require('./util/database');
 
 const app = express();
 
@@ -17,32 +18,30 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(rootDir, 'public')));
 
-// if we will not use promise, then we have to use callback
-// EG: db.execute('SELECT * FROM products', cb => {})
-
-// promises helps us to write cleaner code and gives us then() nad catch() block. both the blocks will take anomous function and will execute one of them according to the condition
-// .then() when request is successful
-// .cath() in case of error
-// db.execute('SELECT * FROM products')
-//   .then((result) => {
-//     console.log(result);
-//   })
-//   .catch(err => console.log(err));
-
-  // .then will be executes and gives us an array which has 2 sub arrays
-  // first array is the result from database and second array hold the metadata 
-
-// thus extrating seprately
-// db.execute('SELECT * FROM products')
-//   .then((result) => {
-//     console.log(result[0]);
-//     console.log(result[1]);
-//   })
-//   .catch(err => console.log(err));
-
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+
+// now our models are created, we have to tell sequelize to create table in database as specified. 
+// if tables are already present, it should not override it. but if we want, we can tell dequelize to do so
+
+// there is a special method, sync method
+// sync method have to look all the methdos, we define (products model in products.js). so its aware of all the models and then create tables for them 
+// sequelize.sync();
+// thus, it syncs all the models to the db(database) by creating appropiate tables and relations, if we have defined
+// sequelize will give promise
+
+sequelize.sync()
+.then(result => {
+  console.log('result' ,result);
+  // if sync is successfull, then server should start
+  app.listen(3000);
+})
+.catch(err => {
+  console.log('err', err);
+})
+
+// thus, if promise is successful, it will create a tabel 'products' eventhough we specified 'product' in our model.
+// along with those fields it will also create 2 from fields by default ie, createdAt and updatedAt. if we dont want, we can tell sequelized not to make.
--- model/product.js
+++ model/product.js
@@ -32,30 +32,19 @@ module.exports = class Product {
   }
 };
 
-******************************************************************/
+*****************************************************************/
 
-// we will recreate our product model from scratch
-
-// importing sequelize class
 const Sequelize = require('sequelize');
 
-// importing sequelize object
 const sequelize = require('../util/database');
-// it is not only a connection pool but is fully configured sequelize environment which does have a connection pool but have other features of sequelize package .active
 
-// defining our new model by "define()" which takes some arguments
-// 1st: modelName - it is typically in lowercase
-// 2nd: define the structure of our model, which will automatically create database tabel. this will be JS object and in this we define the attributes or fields our tabel should have 
 const Product = sequelize.define('product', {
-  // here defining the attrivutes wgich table should have
   id : {
-    // defining the properties if "id" field
     type: Sequelize.INTEGER,
     allowNull : false,
     autoIncrement : true,
     primaryKey : true
   },
-  // if we have to define only type of attribute it should be, we can do it in one line without creating object
   title : Sequelize.STRING,
   price : {
     type: Sequelize.DOUBLE,
@@ -71,5 +60,4 @@ const Product = sequelize.define('product', {
   }
 });
 
-// now we have set up our model and its ready to be used
 module.exports = Product;
