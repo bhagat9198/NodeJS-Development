dc6ab79010c10bf66eb233f615cf3161d1947769
Bhagat Singh <bhagatsingh.17is@saividya.ac.in>
2020-01-28 01:47:54 +0530

    221 Storing All Order Related Data

--- controller/shop.js
+++ controller/shop.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const Product = require('../model/product');
-// importing the order model
 const Order = require('../model/order');
 
 exports.getIndex = (req, res, next) => {
@@ -78,7 +77,6 @@ exports.postCart = (req, res, next) => {
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const productID = req.body.productId;
-  // no change
   req.user.deleteItemsFromCart(productID)
   .then((result) => {    
     res.redirect('/cart');
@@ -101,30 +99,19 @@ exports.getOrders = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
-
-  // as we have defined our order schema. to use that model, we should call it by creating the object on to it and then initilising that object
-  // const order = new Order({
-  //   user: {
-  //     username: req.user.username,
-  //     // userId: req.user._id
-  //     // or
-  //     userId: req.user
-  //     // it will automatically take the userId from the user object
-  //   },
-  //   products: [
-  //     // product:  it should have all the product information
-  //     // quantity: it should have quantity of that product
-  //   ]
-  // })
-
-  // to get the whole product info:-
   req.user
   .populate('cart.items.productId')
   .execPopulate()
   .then(user => {
-    // as we need a object with detals of specific products and its quantity. thus we have to iterate over the array 'items' which contains the quantity and details
     const products = user.cart.items.map(i => {
-      return {quantity: i.quantity, product: i.productId}
+      // priviously, "product: i.productId" was giving us only producyId even though we need all product details which were shown in console.log
+      // so we created one seprated object where all product details will be stored.
+      // ...(spread operator): it will gives out all the data which is within it. atlast productId is an object containing all the data of product
+      // _doc property: even though in console.log of productId we cant see other data , but productId will have lot of metadata attach to it which is of no use to us. hence to omit all that unnessary metadata, we are using '_doc' property.
+      // works fine
+      // return {quantity: i.quantity, product: {...i.productId}};
+      return {quantity: i.quantity, product: {...i.productId._doc}};
+
     });
     console.log(products);
     const order = new Order({
