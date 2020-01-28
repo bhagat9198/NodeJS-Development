0d7975b0f7de6ba3ced8b169f2a54b600eacc6e5
Bhagat Singh <bhagatsingh.17is@saividya.ac.in>
2020-01-28 16:25:49 +0530

    229 Creating the Login Form

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 const rootDir = require('./util/path');
 const errorController = require('./controller/error');
 const User = require('./model/user');
@@ -29,6 +30,7 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 app.use(errorController.get404);
 
 
