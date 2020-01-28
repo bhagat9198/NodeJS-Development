55113b87962917a7cb005ac638af9d06d211e987
Bhagat Singh <bhagatsingh.17is@saividya.ac.in>
2020-01-28 11:11:42 +0530

    228 The Current Project Status

--- controller/shop.js
+++ controller/shop.js
@@ -87,7 +87,7 @@ exports.postCartDeleteProduct = (req, res, next) => {
 exports.getOrders = (req, res, next) => {
   Order.find({'user.userId': req.user._id})
   .then(orders => {
-    console.log(orders);
+    // console.log(orders);
     res.render('shop/orders', {
       pageTitle: 'Orders',
       path: '/orders',
