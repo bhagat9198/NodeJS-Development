e6e6bc6418c0dd1af466b754ad7d903c844d4f31
Bhagat Singh <bhagatsingh.17is@saividya.ac.in>
2020-01-30 12:42:28 +0530

    253 Adding the Signin Functionality

--- controller/auth.js
+++ controller/auth.js
@@ -19,14 +19,46 @@ exports.getSignup = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById('5e2f196a2822d66dbc7f072d')
+  // we no loger want to find dummy user, but the user who have entered email amd password on login page
+  // User.findById('5e2f196a2822d66dbc7f072d')
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({email: email})
     .then(user => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save(err => {
-        console.log(err);
-        res.redirect('/');
-      });
+      // if user with that email is not found
+      if(!user) {
+        return res.redirect('/login');
+      }
+      // if user with that email id found, check for the password if it correct or not
+      // it will give out promise
+      // 1st arg: password to be matched 2nd arg: hashed password with which plain text password should be matched
+      bcrypt.compare(password, user.password)
+      .then(doMatch => {
+        // it will give us true or false value
+        // if password got matched
+        if(doMatch) {
+          req.session.isLoggedIn = true;
+          req.session.user = user;
+          return req.session.save(err => {
+            console.log(err);
+            res.redirect('/');
+          });
+        } else {
+          return res.redirect('/login');
+        }
+      })
+      // if not able to match the passwords because of some problem, then catch block will be executed
+      .catch(err => console.log(err));
+
+    //   req.session.isLoggedIn = true;
+    //   req.session.user = user;
+    //   req.session.save(err => {
+    //     console.log(err);
+    //     res.redirect('/');
+    //   });
+    // })
+    // .catch(err => console.log(err));
     })
     .catch(err => console.log(err));
 };
@@ -38,14 +70,9 @@ exports.postSignup = (req, res, next) => {
   User.findOne({email: email})
   .then(userData => {
     if(userData) {
-      // if we add signing up with exisiting email, we are getting an error. " password: MongooseError [ValidatorError]: Path `password` is required."
-
-      // this is because, our condition is within 'if' block, hence after returning, it movoes to next "then" block where we are storing hashed password.
-      // so, instaed of putting then() bloack outside, we will put it within this block. so after return, it should go to catch block if there is any errors.
       return res.redirect('/signup')
     }
     return bcrypt.hash(password,6)
-    // nested then blocks
     .then(hashedPassword => {
       const user = new User({
         email: email,
