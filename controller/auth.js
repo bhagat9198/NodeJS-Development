db14d4e9616ccdc65b3e9f6395456624c6223cc1
Bhagat Singh <bhagatsingh.17is@saividya.ac.in>
2020-01-31 23:46:33 +0530

    268 Using SendGrid

--- controller/auth.js
+++ controller/auth.js
@@ -27,6 +27,18 @@ exports.getSignup = (req, res, next) => {
   } else {
     message = null;
   }
+  // using Twilio SendGrid's v3 Node.js Library
+  // https://github.com/sendgrid/sendgrid-nodejs
+  // const sgMail = require('@sendgrid/mail');
+  // sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  // const msg = {
+  //   to: 'test@example.com',
+  //   from: 'test@example.com',
+  //   subject: 'Sending with Twilio SendGrid is Fun',
+  //   text: 'and easy to do anywhere, even with Node.js',
+  //   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  // };
+  // sgMail.send(msg).then(()=> {}).catch(err => console.log(err));
   res.render('auth/signup', {
     path: '/signup',
     pageTitle: 'Signup',
