b07375eae3b0c5336ce607c315b7bfbd397a0fc7
Bhagat Singh <bhagatsingh.17is@saividya.ac.in>
2020-02-09 11:09:49 +0530

    029 Sending Responses

--- basics.js
+++ basics.js
@@ -2,8 +2,16 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
-    // terminating the manually. "process" is global keyword
-    process.exit();
+
+    // sending responses
+    // we cant write all html data within "write()" as it take small chuck of data at a time.
+    res.write('<html>');
+    res.write('<head><title>My App</title></head>');
+    res.write('<body><h1>Hello World</h1></body>');
+    res.write('</html>');
+    // to tell the server that we have finished writing our response. else server will keep waiting for more response
+    res.end();
+
 });
 
 server.listen(3000);
