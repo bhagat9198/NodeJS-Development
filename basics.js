8ec850499d08837b7bc3c5c370a7e4a18f9f5a34
Bhagat Singh <bhagatsingh.17is@saividya.ac.in>
2020-02-09 12:02:39 +0530

    033 Parsing Request Bodies

--- basics.js
+++ basics.js
@@ -2,36 +2,44 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    const url = req.url;
-    if(url === '/') {
-        res.write('<html>');
-        res.write('<head><title>My App</title></head>');
-        res.write('<body><form method="POST" action="/message">');
-        res.write('<input type="text" name="box">')
-        res.write('<button type="submit">Add Item</button>');
-        res.write('</form></body>');
-        res.write('<html>');
-        // res.end();
-        return res.end();
-    }
+  const url = req.url;
+  if(url === '/') {
+      res.write('<html>');
+      res.write('<head><title>My App</title></head>');
+      res.write('<body><form method="POST" action="/message">');
+      res.write('<input type="text" name="box">')
+      res.write('<button type="submit">Add Item</button>');
+      res.write('</form></body>');
+      res.write('<html>');
+      // res.end();
+      return res.end();
+  }
 
-    if(url === '/message' && req.method === 'POST') {
-        // using file system to write down in file
-        // fs.writeFile()
-        // 1st arg: telling where to write 2nd arg: what to write down in file
-        fs.writeFileSync('message.txt', 'Dummy Text');
-        res.statusCode = 302; //302 code is for re-directing
-        res.setHeader('Content-Type','text/html');
-        res.setHeader('Location','/');
-        res.write('<h1>Message received</h1>');
-        return res.end();
-    }
+  if(url === '/message' && req.method === 'POST') {
+    const body =[];
+    req.on('data', (chunk) => {
+        console.log(chunk);
+        // <Buffer 62 6f 78 3d 68 65 6c 6c 6f>
+        body.push(chunk);
+    });
+    req.on('end', () => {
+        const parsedBody = Buffer.concat(body).toString();
+        console.log(parsedBody); 
+        // box=hello
+        const message = parsedBody.split('=')[1];
+        fs.writeFileSync('message.txt', message);
+    })
+    res.statusCode = 302; 
+    res.setHeader('Content-Type','text/html');
+    res.setHeader('Location','/');
+    return res.end();
+  }
 
-    res.write('<html>');
-    res.write('<head><title>My App</title></head>');
-    res.write('<body><h1>Hello from NodeJs</h1></body>');
-    res.write('</html>');
-    res.end();
+  res.write('<html>');
+  res.write('<head><title>My App</title></head>');
+  res.write('<body><h1>Hello from NodeJs</h1></body>');
+  res.write('</html>');
+  res.end();
 
 });
 
