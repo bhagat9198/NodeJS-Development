0eb9fe0d90040984166cd5a529d4db5e466238ba
Bhagat Singh <bhagatsingh.17is@saividya.ac.in>
2020-02-10 13:13:55 +0530

    067 Filtering Paths

--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,11 @@ const express = require('express');
 
 const routes = express.Router();
 
-routes.get('/admin/add-products', (req, res, next) => {
+routes.get('/add-products', (req, res, next) => {
   res.send('<form action="/admin/add-products" method="POST"><input type="text" name="title"><button type="submit">Add</button></form>');
 });
 
-routes.post('/admin/add-products', (req, res, next) => {
+routes.post('/add-products', (req, res, next) => {
   console.log(req.body);
   res.redirect('/');
 });
