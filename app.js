82df3b9b6c9b1edf29dcb40da7d1446652d49626
Bhagat Singh <bhagatsingh.17is@saividya.ac.in>
2020-02-10 12:47:46 +0530

    064 Limiting Middleware Execution to POST Requests

--- app.js
+++ app.js
@@ -1,34 +1,25 @@
 const http = require('http');
 
 const express = require('express');
-// requering
+
 const bodyParser = require('body-parser');
 
 const routes = require('./routes');
 
 const app = express();
 
-// configuring body parser
-// by default it will have use method i.e, (req,res,next) => {}
-//            and next() will be pass it another middleware below it
-// app.use(bodyParser.urlencoded());
-
-
-// extended: true means that it should able to parse non-default features 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use('/add-product', (req, res, next) => {
-  // console.log("Another middleware");
+app.get('/add-product', (req, res, next) => {
   res.send('<form method="POST" action="/product"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
-app.use('/product', (req, res, next) => {
+app.post('/product', (req, res, next) => {
   console.log(req.body);
   res.redirect('/');
 })
 
-app.use('/', (req, res, next) => {
-  // console.log("home middleware");
+app.get('/', (req, res, next) => {
   res.send('<h1> Home Page</h1>');
 });
 
