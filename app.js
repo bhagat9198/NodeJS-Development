d4c8789e8167756be3136cb999355a9ac34e798a
Bhagat Singh <bhagatsingh.17is@saividya.ac.in>
2020-02-10 12:26:26 +0530

    062 Handling Different Routes

--- app.js
+++ app.js
@@ -6,17 +6,24 @@ const routes = require('./routes');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("First middleware");
-  next();
-}); 
+// app.use((req, res, next) => {
+//   console.log("First middleware");
+//   next();
+// }); 
 
-app.use((req, res, next) => {
-  console.log("Second Middleware");
-  res.send('<h1>Hello from Express!</h1>');
-})
+// app.use((req, res, next) => {
+//   console.log("Second Middleware");
+//   res.send('<h1>Hello from Express!</h1>');
+// })
 
-// const server = http.createServer(app);
-// server.listen(3000);
+app.use('/add-product', (req, res, next) => {
+  console.log("Another middleware");
+  res.send('<h1>Add Product Page</h1>');
+});
+
+app.use('/', (req, res, next) => {
+  console.log("home middleware");
+  res.send('<h1> Home Page</h1>');
+});
 
 app.listen(3000);
