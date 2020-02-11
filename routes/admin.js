3b6f79ca9ad4ad4b63a37b873afb8131f3eb3901
Bhagat Singh <bhagatsingh.17is@saividya.ac.in>
2020-02-11 12:48:34 +0530

    069 Serving HTML Pages

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,16 @@
+const path = require('path');
+
 const express = require('express');
 
 const routes = express.Router();
 
 routes.get('/add-products', (req, res, next) => {
-  res.send('<form action="/admin/add-products" method="POST"><input type="text" name="title"><button type="submit">Add</button></form>');
+  // res.send('<form action="/admin/add-products" method="POST"><input type="text" name="title"><button type="submit">Add</button></form>');
+
+  // res.sendFile('/views/add-product.html');
+  res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
+
+
 });
 
 routes.post('/add-products', (req, res, next) => {
