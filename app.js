a4457355ff9a5a57d743ffc49fefa004cedf953f
Bhagat Singh <bhagatsingh.17is@saividya.ac.in>
2020-02-11 13:07:18 +0530

    070 Returning a 404 Page

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -12,7 +14,8 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.send('<h1>Page Not Found</h1>');
+  // res.send('<h1>Page Not Found</h1>');
+  res.sendFile(path.join(__dirname, 'views', '404.html'));
 })
 
 app.listen(3000);
