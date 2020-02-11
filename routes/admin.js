b23e386785d977451cad1251977375e0d4d5176d
Bhagat Singh <bhagatsingh.17is@saividya.ac.in>
2020-02-11 12:59:02 +0530

    069.1 Serving HTML Pages

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,18 @@
+// core module
 const path = require('path');
 
+// 3rd party module
 const express = require('express');
+
+// user defined module
+const rootDir = require('../util/path');
 
 const routes = express.Router();
 
 routes.get('/add-products', (req, res, next) => {
-  // res.send('<form action="/admin/add-products" method="POST"><input type="text" name="title"><button type="submit">Add</button></form>');
+  // res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
 
-  // res.sendFile('/views/add-product.html');
-  res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
-
-
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 routes.post('/add-products', (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -2,12 +2,14 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../util/path');
+
 const routes = express.Router();
 
 routes.get('/', (req, res, next) => {
-  // res.send('<html><head><title>Main App</title></head><body><h1>Home Page</h1></body></html>');
-
-  res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
+  // res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
+  
+  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = routes;
