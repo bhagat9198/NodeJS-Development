0bf0f63e35959eaab83c569c2d3f3021dcf75848
Bhagat Singh <bhagatsingh.17is@saividya.ac.in>
2020-02-11 13:24:48 +0530

    073 Serving Files Statically

--- app.js
+++ app.js
@@ -11,15 +11,20 @@ const shopRoutes = require('./routes/shop');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
+// craeting the static path to "public folder"
+// we can have mutiple static, then file which match first will be rendered to file which is requesting
+// app.use(express.static(path.join(rootDir, 'public', 'css')));
+app.use(express.static(path.join(rootDir, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// we can add more then one static file if we have
+// app.use(express.static(path.join(rootDir, 'publics')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  // res.sendFile(path.join(__dirname, 'views', '404.html'));
-
   res.sendFile(path.join(rootDir, 'views', '404.html'));
-
 })
 
 app.listen(3000);
