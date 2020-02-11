74b4423501cb38a30ede4819d0198bd370eb640e
Bhagat Singh <bhagatsingh.17is@saividya.ac.in>
2020-02-11 13:13:20 +0530

    071 Using a Helper Function for Navigation

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const rootDir = require('./util/path');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -12,7 +16,10 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.send('<h1>Page Not Found</h1>');
+  // res.sendFile(path.join(__dirname, 'views', '404.html'));
+
+  res.sendFile(path.join(rootDir, 'views', '404.html'));
+
 })
 
 app.listen(3000);
